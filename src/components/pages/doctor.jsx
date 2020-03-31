e8d1323349af35ee5276f73078fd44e1beaba36e
Adam Czarnik <a.czarnik@me.com>
2020-03-31 13:10:58 -0700

    adding doctor file with buttons

---
+++ src/components/pages/doctor.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Logo from "./erpharmtechgrayer.png";
+//import Cart from "./cart.png";
+import {Link} from "react-router-dom";
+
+export class Doctor extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "test",
+        };
+        // const { username } = this.props.match.params.username
+    }
+
+    render() {
+        return (
+           <div className = "body">
+               <div className = "navBar">
+                <nav>
+                    <div className = "img" id = "logo">
+                        <img src={Logo} alt="Logo"/>
+                    </div>
+                    <h1 className = "welcomeMan">
+                        Welcome back {this.state.username}!
+                    </h1>
+                </nav>
+               </div>
+               <div className = "menu" id = "doctor">
+                    <button type = "button" id = "manMenuButton">View<br />In-Stock<br/>Drugs</button>
+                    <button type = "button" id = "manMenuButton">View<br/>Incoming<br/>Drugs</button>
+                    <button type = "button" id = "manMenuButton">Search <br/>for a<br />Drug</button>
+                    <button type = "button" id = "manMenuButton">View<br />Similar<br/>Drugs</button>
+               </div>
+           </div>
+        );
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
